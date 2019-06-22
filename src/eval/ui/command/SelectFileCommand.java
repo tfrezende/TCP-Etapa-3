f75a2123e98612256c1ade73fc250b5634aaaec4
Thalles Rezende <tfrezende@inf.ufrgs.br>
2019-06-22 03:16:06 -0300

    Command de Selecionar Arquivos completo

--- src/eval/ui/command/SelectFileCommand.java
+++ src/eval/ui/command/SelectFileCommand.java
@@ -1,9 +1,16 @@
 package eval.ui.command;
 
+import java.util.List;
+
+import eval.business.BusinessException;
+import eval.business.domain.EvalGroup;
+import eval.business.domain.Product;
 import eval.data.Database;
 import eval.ui.CatalogueInterface;
+import eval.ui.UIUtils;
 
 public class SelectFileCommand extends Command {
+	private List<EvalGroup> groups;
 
 	public SelectFileCommand(CatalogueInterface catalogueInterface, Database database) {
 		super(catalogueInterface, database);
@@ -11,6 +18,50 @@ public class SelectFileCommand extends Command {
 
 	@Override
 	public void execute() throws Exception {
+		groups = (List<EvalGroup>) database.getAllGroups();
+		List<Product> acceptableProducts;
+		List<Product> notAcceptableProducts;
 		
+		System.out.println("--- Grupos registrados ---");		
+		for(EvalGroup group : groups) {
+			System.out.println(group.getName());
+		}
+		
+		String groupName = UIUtils.INSTANCE.readString("message.ask.group");
+		
+		EvalGroup evalGroup = null;
+		
+		for(EvalGroup group : groups) {
+			if(group.getName() == groupName) {
+				evalGroup = group;
+			}				
+		}
+		
+		if (!isGroupValid(evalGroup)) {
+			throw new BusinessException("exception.invalid.group");
+		}
+		else {
+			acceptableProducts = evalGroup.getAcceptableProducts();
+			notAcceptableProducts = evalGroup.getNotAcceptableProducts();
+			
+			System.out.println("selection.acceptable.quality");			
+			printProducts(acceptableProducts);
+			
+			System.out.println("selection.unacceptable.quality");
+			printProducts(notAcceptableProducts);
+		}
+		
+	}
+	
+	public boolean isGroupValid(EvalGroup group) {		
+		return (group.isAllocated() && group.isEvaluationDone());
+	}
+	
+	public void printProducts(List<Product> list) {
+		System.out.println("ID\tNOME\tNOTA MÉDIA");	
+		for(Product element : list) {
+			System.out.println(element.getId() + "/t" + element.getName() + "/t" +
+			element.getAverageScore());
+		}
 	}
 }
